f2927df1844e9d594a559cb089b6fc49f4d457cf
totallyrin <lucy.woloszczuk@gmail.com>
2020-11-07 13:37:19 -0500

    added docstring, comments to make coding more clear

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-# this is a code
+"""
+Code for QWIC hackHer 2020.
+
+07/11/2020
+"""
 
 import tkinter as tk
 
 window = tk.Tk()
+# GUI code goes before window.mainloop()
+
 window.mainloop()
-
-print("Code is working")
-
-# this is a comment
