4b06b3a99a69f2dc4a05038d33bc280ad8ead3d8
totallyrin <lucy.woloszczuk@gmail.com>
2020-11-07 13:43:47 -0500

    added some code

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ Code for QWIC hackHer 2020.
 import tkinter as tk
 
 window = tk.Tk()
-# GUI code goes before window.mainloop()
+# set window size, width x height in pixels
+window.geometry("800x400")
+title = tk.Label(window, text = "hackHer2020")
+title.pack()
 
+# GUI code goes before window.mainloop()
 window.mainloop()
